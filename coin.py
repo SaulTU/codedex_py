36ee6e67ff32107c757155ac6471226f85013ba5
SAUL TORRES <38293360+SaulTU@users.noreply.github.com>
2023-05-17 00:00:20 -0600

    Create coin.py
    reto conversion de moneda

---
+++ coin.py
@@ -0,0 +1,10 @@
+yuan = int(input('What do you have left in yuan?'))
+yen = int(input('What do you have left in yuan?'))
+won = int(input('What do you have left in yuan?'))
+
+us = 0
+us = us + (yuan*.143)
+us = us + (yen*.0073)
+us = us + (won*.00075)
+
+print(us)
